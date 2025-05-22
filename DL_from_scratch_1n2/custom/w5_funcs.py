ba595befee3bb9c7cc57c814ba8b4b4c25094c91
maxjo020418 <maxjo020418@gmail.com>
2025-05-22 17:29:59 +0900

    W11 complete

--- DL_from_scratch_1n2/custom/w5_funcs.py
+++ DL_from_scratch_1n2/custom/w5_funcs.py
@@ -13,7 +13,6 @@ from common.layers import *
 from common.gradient import numerical_gradient
 from collections import OrderedDict
 
-
 class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
